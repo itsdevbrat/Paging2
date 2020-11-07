1296a16afa137614665949d3c0473b061a070f8b
Devbrat Dash <48611876+itsdevbrat@users.noreply.github.com>
2020-11-07 16:54:46 +0530

    Update PostRepository.java

--- app/src/main/java/com/example/pagingapp/model/repositories/PostRepository.java
+++ app/src/main/java/com/example/pagingapp/model/repositories/PostRepository.java
@@ -25,21 +25,28 @@ public class PostRepository {
     public PostRepository(Api api) {
         this.api = api;
         postsDataSourceFactory = new PostsDataSourceFactory(api);
+        
+        //IMPORTANT:
+        //since we can only access PostsDataSourceFactory which has no method 
+        //to access the loading state present within PostsDataSource
+        //we use this Transformations API which helps us to get the load state livedata from PostsDataSource 
+        //which we can let other classes to access it from here by creating a getter
         loadState = Transformations.switchMap(postsDataSourceFactory.getPostsDataSourceMutableLiveData(), PostsDataSource::getLoadState);
+        
     }
 
     public LiveData<PagedList<Post>> getPagedList() {
 
-
+        //There are some parameters that we can config according to our use case
         PagedList.Config config = new PagedList.Config.Builder()
                 .setEnablePlaceholders(false)
-                .setPageSize(5)
-                .setPrefetchDistance(1)//300
-                .setMaxSize(10)//Maximum size must be at least pageSize + 2*prefetchDist
-                .build();
+                .setPageSize(5) //Configures how many data items in one page to be supplied to recycler view
+                .setPrefetchDistance(1) // in first call how many pages to load 
+                .setMaxSize(10) //Maximum PagedList size must be at least pageSize + 2*prefetchDist
+                .build();                
 
         return new LivePagedListBuilder<>(postsDataSourceFactory, config)
-                .setFetchExecutor(Executors.newFixedThreadPool(5))
+                .setFetchExecutor(Executors.newFixedThreadPool(5)) // Use five threads to do the fetching operations 
                 .build();
 
     }
