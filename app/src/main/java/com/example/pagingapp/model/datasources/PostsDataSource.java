c469c333116916c46af0e11fa7f7a54aa1ccc14f
Devbrat Dash <48611876+itsdevbrat@users.noreply.github.com>
2020-11-07 16:53:30 +0530

    Update PostsDataSource.java

--- app/src/main/java/com/example/pagingapp/model/datasources/PostsDataSource.java
+++ app/src/main/java/com/example/pagingapp/model/datasources/PostsDataSource.java
@@ -22,6 +22,8 @@ import retrofit2.Response;
 public class PostsDataSource extends PageKeyedDataSource<Integer, Post> {
 
     Api api;
+    
+    //A livedata which has a integer that holds the state of loading
     MutableLiveData<Integer> loadState;
 
     public PostsDataSource(Api api) {
@@ -29,28 +31,50 @@ public class PostsDataSource extends PageKeyedDataSource<Integer, Post> {
         this.api = api;
     }
 
+    //This below method is called only once and the first method to be called 
+    
     @Override
     public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull final LoadInitialCallback<Integer, Post> callback) {
 
         final int currentPage = 1;
 
+        //setting load state so that the UI can know that progress of data fetching
         loadState.postValue(Constants.ONGOING);
+        
+        //Simple retrofit call to get the first page of data
         api.getPosts(currentPage).enqueue(new Callback<List<Post>>() {
+        
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
                 if (response.isSuccessful()) {
+    
+                    //IMPORTANT: once the first page is load we increment the page count and pass it to callback.onResult()  
                     callback.onResult(response.body(), null, currentPage + 1);
+                    
+                    //setting load state so that the UI can know data fetching is successful
                     loadState.postValue(Constants.SUCCESS);
+                    
                 } else {
+                
+                    //since no data was fetched we pass empty list and dont increment the page number 
+                    //so that it can retry the fetching of 1st page
                     callback.onResult(new ArrayList<>(), null, currentPage);
+                    
+                    //setting load state so that the UI can know data fetching failed
                     loadState.postValue(Constants.FAILED);
                 }
             }
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
-                callback.onResult(new ArrayList<>(), null, currentPage);
-                loadState.postValue(Constants.FAILED);
+            
+                    //since no data was fetched we pass empty list and dont increment the page number 
+                    //so that it can retry the fetching of 1st page
+                    callback.onResult(new ArrayList<>(), null, currentPage);
+                    
+                    //setting load state so that the UI can know data fetching failed
+                    loadState.postValue(Constants.FAILED);
+            
             }
         });
 
@@ -61,10 +85,13 @@ public class PostsDataSource extends PageKeyedDataSource<Integer, Post> {
 
     }
 
+    // this method will be called every time after the loadInitial() is been called 
     @Override
     public void loadAfter(@NonNull LoadParams<Integer> params, @NonNull final LoadCallback<Integer, Post> callback) {
 
+        // we get the current page from params
         final int currentPage = params.key;
+        
         loadState.postValue(Constants.ONGOING);
 
         api.getPosts(currentPage).enqueue(new Callback<List<Post>>() {
@@ -88,6 +115,7 @@ public class PostsDataSource extends PageKeyedDataSource<Integer, Post> {
         });
     }
 
+    //UI will call this method to get the livedata of loading, subscribe to it, and update the UI state accordingly 
     public MutableLiveData<Integer> getLoadState() {
         return loadState;
     }
